71546723aba1e47f8dcc5646ce14ea57365dd6c7
Paulo Valente <polvalente@gmail.com>
2019-08-05 02:16:09 -0300

    feat: implement ignore blocks to allow for proper commented code and string text ignoring and improve elixir syntax matching
    feat: support other languages in ignore blocks

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,41 +1,51 @@
+## 0.7.0
+
+By https://github.com/polvalente
+
+- Improve Elixir support
+- Add code block ignore functionality
+  - Add said ignore blocks for some of the supported languages
+- Allow matching for the first character in the document
+
 ## 0.6.0
 
 By https://github.com/spgennard:
-* Support for case insensitive language
-* Cobol support
+
+- Support for case insensitive language
+- Cobol support
 
 ## 0.5.4
 
-* Fixes for Verilog
+- Fixes for Verilog
 
 ## 0.5.3
 
-* Fixes for VHDL
+- Fixes for VHDL
 
 ## 0.5.2
 
-* Fix do-end for crystal
+- Fix do-end for crystal
 
 ## 0.5.1
 
-* Add loop keyword for ruby
-* Add with and quote keywords for Elixir (#4)
+- Add loop keyword for ruby
+- Add with and quote keywords for Elixir (#4)
 
 ## 0.5.0
 
-* Verilog support
+- Verilog support
 
 ## 0.4.0
 
-* Upgrade dependencies
-* Add a dedicated changelog
-* Add support for the ruby keyword unless
-* Ruby inline if & unless support (Fix #2)
+- Upgrade dependencies
+- Add a dedicated changelog
+- Add support for the ruby keyword unless
+- Ruby inline if & unless support (Fix #2)
 
 ### 0.3.0
 
-* Add support for Crystal language
-* Add support for Shell language
+- Add support for Crystal language
+- Add support for Shell language
 
 ### 0.2.0
 
--- src/languages.ts
+++ src/languages.ts
@@ -1,196 +1,234 @@
 export const languages: {
-    [index: string]: {
-        caseSensitive: boolean,
-        inlineOpenTokens: Array<string>,
-        openTokens: Array<string>,
-        closeTokens: Array<string>,
-        neutralTokens: Array<string>
-    }
+  [index: string]: {
+    caseSensitive: boolean;
+    ignoreInDelimiters?: Array<{
+      open: string;
+      close: string;
+    }>;
+    inlineOpenTokens: Array<string>;
+    openTokens: Array<string>;
+    closeTokens: Array<string>;
+    neutralTokens: Array<string>;
+  };
 } = {
-    ruby: {
-        caseSensitive: true,
-        inlineOpenTokens: [ // Allow stuff like return toto if tutu
-            "if",
-            "unless",
-        ],
-        openTokens: [
-            "class",
-            "module",
-            "def",
-            "while",
-            "do",
-            "case",
-            "begin",
-            "loop",
-        ],
-        closeTokens: [
-            "end",
-        ],
-        neutralTokens: [
-            "elsif",
-            "else",
-            "when",
-            "rescue"
-        ]
-    },
-    lua: {
-        caseSensitive: true,
-        inlineOpenTokens: [],
-        openTokens: [
-            "function",
-            "if",
-            "while",
-            "for"
-        ],
-        closeTokens: [
-            "end",
-        ],
-        neutralTokens: [
-            "then",
-            "else",
-            "elseif",
-        ]
-    },
-    elixir: {
-        caseSensitive: true,
-        inlineOpenTokens: [],
-        openTokens: [
-            "defmodule",
-            "defmacro",
-            "def",
-            "if",
-            "while",
-            "for",
-            "case",
-            "cond",
-            "unless",
-            "try",
-            "quote",
-            "with",
-        ],
-        closeTokens: [
-            "end",
-        ],
-        neutralTokens: [
-            "do",
-            "else",
-            "elseif",
-            "rescue",
-            "after"
-        ]
-    },
-    shellscript: {
-        caseSensitive: true,
-        inlineOpenTokens: [],
-        openTokens: [
-            "for",
-            "if",
-            "while",
-            "until"
-        ],
-        closeTokens: [
-            "fi",
-            "done"
-        ],
-        neutralTokens: [
-            "do",
-            "in",
-            "then",
-            "else"
-        ]
-    },
-    verilog: {
-        caseSensitive: true,
-        inlineOpenTokens: [],
-        openTokens: [
-            "module",
-            "case",
-            "begin"
-        ],
-        closeTokens: [
-            "end",
-            "endmodule",
-            "endcase"
-        ],
-        neutralTokens: [
-        ]
-    },
-    vhdl: {
-        caseSensitive: true,
-        inlineOpenTokens: [],
-        openTokens: [
-            "entity",
-            "component",
-            "case",
-            "begin"
-        ],
-        closeTokens: [
-            "end",
-            "endcase"
-        ],
-        neutralTokens: [
-        ]
-    },
-    crystal: {
-        caseSensitive: true,
-        inlineOpenTokens: [],
-        openTokens: [
-            "class",
-            "module",
-            "struct",
-            "enum",
-            "macro",
-            "def",
-            "if",
-            "while",
-            "case",
-            "unless",
-            "until",
-            "try",
-            "do",
-        ],
-        closeTokens: [
-            "end",
-        ],
-        neutralTokens: [
-            "else",
-            "elseif",
-            "rescue",
-            "ensure"
-        ]
-    },
-    COBOL: {
-        caseSensitive: false,
-        inlineOpenTokens: [],
-        openTokens: [
-            "program-id",
-            "perform",
-            "evalute",
-            "read",
-            "perform",
-            "call",
-            "evaluate",
-            "if",
-            "method-id"
-        ],
-        closeTokens: [
-            "end-perform",
-            "end-evalute",
-            "end-read",
-            "end-perform",
-            "end-call",
-            "end-evaluate",
-            "end-if",
-            "end program",
-            "end method"
-        ],
-        neutralTokens: [
-            "entry",
-            "else",
-            "when",
-            "procedure division",
-            "goback",
-            "exit program"
-        ]
-    },
+  ruby: {
+    caseSensitive: true,
+    ignoreInDelimiters: [
+      {
+        open: "#",
+        close: "\n"
+      },
+      { open: "=begin", close: "=end" },
+      {
+        open: '"',
+        close: '"'
+      }
+    ],
+    inlineOpenTokens: [
+      // Allow stuff like return toto if tutu
+      "if",
+      "unless"
+    ],
+    openTokens: [
+      "class",
+      "module",
+      "def",
+      "while",
+      "do",
+      "case",
+      "begin",
+      "loop"
+    ],
+    closeTokens: ["end"],
+    neutralTokens: ["elsif", "else", "when", "rescue"]
+  },
+  lua: {
+    caseSensitive: true,
+    ignoreInDelimiters: [
+      {
+        open: '"',
+        close: '"'
+      },
+      {
+        open: "'",
+        close: "'"
+      },
+      {
+        open: "--\\[\\[",
+        close: "--\\]\\]"
+      },
+      {
+        open: "--",
+        close: "\n"
+      }
+    ],
+    inlineOpenTokens: [],
+    openTokens: ["function", "if", "while", "for"],
+    closeTokens: ["end"],
+    neutralTokens: ["then", "else", "elseif"]
+  },
+  elixir: {
+    caseSensitive: true,
+    ignoreInDelimiters: [
+      {
+        open: "#",
+        close: "\n"
+      },
+      {
+        open: '"""',
+        close: '"""'
+      },
+      {
+        open: '"',
+        close: '"'
+      },
+      {
+        open: "'",
+        close: "'"
+      }
+    ],
+    inlineOpenTokens: [],
+    openTokens: [
+      "fn",
+      "defmodule",
+      "defmacro(?=.+do)",
+      "defmacrop(?=.+do)",
+      "def(?=.+do)",
+      "defp(?=.+do)",
+      "if",
+      "while",
+      "for",
+      "case",
+      "cond",
+      "unless",
+      "try",
+      "quote",
+      "with",
+      "defprotocol",
+      "defimpl",
+      "schema",
+      "embedded_schema",
+      "resources(?=.+do)",
+      "scope(?=.+do)"
+    ],
+    closeTokens: ["end", ", do"],
+    neutralTokens: ["do", "else", "elseif", "rescue", "after"]
+  },
+  shellscript: {
+    caseSensitive: true,
+    ignoreInDelimiters: [
+      {
+        open: "#",
+        close: "\n"
+      },
+      {
+        open: '"',
+        close: '"'
+      },
+      {
+        open: "'",
+        close: "'"
+      }
+    ],
+    inlineOpenTokens: [],
+    openTokens: ["for", "if", "while", "until"],
+    closeTokens: ["fi", "done"],
+    neutralTokens: ["do", "in", "then", "else"]
+  },
+  verilog: {
+    caseSensitive: true,
+    ignoreInDelimiters: [
+      {
+        open: "/\\*",
+        close: "\\*/"
+      },
+      {
+        open: "//",
+        close: "\n"
+      }
+    ],
+    inlineOpenTokens: [],
+    openTokens: ["module", "case", "begin"],
+    closeTokens: ["end", "endmodule", "endcase"],
+    neutralTokens: []
+  },
+  vhdl: {
+    caseSensitive: true,
+    ignoreInDelimiters: [
+      {
+        open: "--",
+        close: "\n"
+      }
+    ],
+    inlineOpenTokens: [],
+    openTokens: ["entity", "component", "case", "begin"],
+    closeTokens: ["end", "endcase"],
+    neutralTokens: []
+  },
+  crystal: {
+    caseSensitive: true,
+    ignoreInDelimiters: [
+      {
+        open: '"',
+        close: '"'
+      },
+      {
+        open: "#",
+        close: "\n"
+      }
+    ],
+
+    inlineOpenTokens: [],
+    openTokens: [
+      "class",
+      "module",
+      "struct",
+      "enum",
+      "macro",
+      "def",
+      "if",
+      "while",
+      "case",
+      "unless",
+      "until",
+      "try",
+      "do"
+    ],
+    closeTokens: ["end"],
+    neutralTokens: ["else", "elseif", "rescue", "ensure"]
+  },
+  COBOL: {
+    caseSensitive: false,
+    inlineOpenTokens: [],
+    openTokens: [
+      "program-id",
+      "perform",
+      "evalute",
+      "read",
+      "perform",
+      "call",
+      "evaluate",
+      "if",
+      "method-id"
+    ],
+    closeTokens: [
+      "end-perform",
+      "end-evalute",
+      "end-read",
+      "end-perform",
+      "end-call",
+      "end-evaluate",
+      "end-if",
+      "end program",
+      "end method"
+    ],
+    neutralTokens: [
+      "entry",
+      "else",
+      "when",
+      "procedure division",
+      "goback",
+      "exit program"
+    ]
+  }
 };
